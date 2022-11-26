6524127e4a9fda1f87edb8b25b5d4cfb982fe204
Kevin Caley <Kevin.R.Caley@gmail.com>
2022-11-26 09:52:09 -0500

    file structure changes

--- app/subs/support.py
+++ app/subs/support.py
@@ -1,9 +1,43 @@
-
+import pandas as pd
+import functools as ft
+from app.config import db
 
 ##Use this for matching our stocks to their appropriate benchmark?
 class Benchmark:
     def __init__(self):
-        pass
+        self.snp='^GSPC'
+        self.dji='^DJI'
 
+    def StockList(self):
+        return pd.read_sql(f"select distinct ticker From setup.equity_pricing order by 1", db)['ticker'].values
 
+    def StockPrices(self, ticker, from_dt, to_dt):
+        stockdata=pd.read_sql(f"select businessdate, ticker, close From setup.equity_pricing where ticker='{ticker}' and businessdate between '{from_dt}' and '{to_dt}' order by 1", db, index_col='businessdate')
+        snpdata=pd.read_sql(f"select businessdate, ticker, close From setup.index_pricing where ticker='{self.snp}' and businessdate between '{from_dt}' and '{to_dt}' order by 1", db, index_col='businessdate')
+        djidata=pd.read_sql(f"select businessdate, ticker, close From setup.index_pricing where ticker='{self.dji}' and businessdate between '{from_dt}' and '{to_dt}' order by 1", db, index_col='businessdate')
+        
+        stockdata['ticker_stdclose']=stockdata.loc[:,'close'].apply(lambda e : (e - stockdata['close'].mean()) / stockdata['close'].std())
+        stockdata=stockdata.rename(columns={"ticker": "ticker", "close": "ticker_close"})
 
+        snpdata['snp_stdclose']=snpdata.loc[:,'close'].apply(lambda i : (i - snpdata['close'].mean()) / snpdata['close'].std())
+        snpdata=snpdata.rename(columns={"ticker": "snp_ticker", "close": "snp_close"})
+
+        djidata['dji_stdclose']=djidata.loc[:,'close'].apply(lambda i : (i - djidata['close'].mean()) / djidata['close'].std())
+        djidata=djidata.rename(columns={"ticker": "dji_ticker", "close": "dji_close"})
+
+        stck_dfs = [stockdata, snpdata, djidata]
+        #inner join on market dates, excluding weekends/holidays
+        df_final = ft.reduce(lambda left, right: pd.merge(left, right, on='businessdate'), stck_dfs) 
+
+        return df_final.reset_index().to_dict(orient='list')
+
+    def SentimentData(self, ticker, from_dt, to_dt):
+        sentimentdata=pd.read_sql(f"""select ticker, created_dt traded_dt, title, selftext, comments
+                                    from setup.reddit_commentary 
+                                    where ticker='{ticker}' 
+                                    and created_dt between '{from_dt}' and '{to_dt}' 
+                                    order by created_dt desc""", db)
+
+        sentimentdata['merged_comments']=sentimentdata[['title','selftext','comments']].stack().groupby(level=0).apply(' '.join)
+
+        return sentimentdata[['ticker','traded_dt','merged_comments']].to_dict(orient='list')
--- app/views.py
+++ app/views.py
@@ -1,55 +1,25 @@
 from app import app
-from app.config import db
 from app.subs.support import Benchmark
 
-import pandas as pd
-import functools as ft
 from flask import render_template, request, jsonify
 
- 
+bench=Benchmark()
+
 @app.route('/', methods=["GET"])
 @app.route('/main', methods=["GET"])
 def index():
     headline="CSE 6242 Social Media Sentiment Analysis"
-    stocklist = pd.read_sql(f"select distinct ticker From setup.equity_pricing order by 1", db)
-    return render_template("main.html", stocklist=stocklist['ticker'].values, headline=headline)
+    return render_template("main.html", stocklist=bench.StockList(), headline=headline)
 
 @app.route("/getstock", methods=['POST'])
 def getstock():
     stockNm=request.get_json()
-    stockdata=pd.read_sql(f"select businessdate, ticker, close From setup.equity_pricing where ticker='{stockNm['stockchoice']}' and businessdate between '{stockNm['from_date']}' and '{stockNm['to_date']}' order by 1", db, index_col='businessdate')
-    snpdata=pd.read_sql(f"select businessdate, ticker, close From setup.index_pricing where ticker='^GSPC' and businessdate between '{stockNm['from_date']}' and '{stockNm['to_date']}' order by 1", db, index_col='businessdate')
-    djidata=pd.read_sql(f"select businessdate, ticker, close From setup.index_pricing where ticker='^DJI' and businessdate between '{stockNm['from_date']}' and '{stockNm['to_date']}' order by 1", db, index_col='businessdate')
-    
-    stockdata['ticker_stdclose']=stockdata.loc[:,'close'].apply(lambda e : (e - stockdata['close'].mean()) / stockdata['close'].std())
-    stockdata=stockdata.rename(columns={"ticker": "ticker", "close": "ticker_close"})
-
-    snpdata['snp_stdclose']=snpdata.loc[:,'close'].apply(lambda i : (i - snpdata['close'].mean()) / snpdata['close'].std())
-    snpdata=snpdata.rename(columns={"ticker": "snp_ticker", "close": "snp_close"})
-
-    djidata['dji_stdclose']=djidata.loc[:,'close'].apply(lambda i : (i - djidata['close'].mean()) / djidata['close'].std())
-    djidata=djidata.rename(columns={"ticker": "dji_ticker", "close": "dji_close"})
-
-    stck_dfs = [stockdata, snpdata, djidata]
-    #inner join on market dates, excluding weekends/holidays
-    df_final = ft.reduce(lambda left, right: pd.merge(left, right, on='businessdate'), stck_dfs) 
-    
-    return jsonify({"stockdata": df_final.reset_index().to_dict(orient='list')})
-
+    return jsonify({"stockdata": bench.StockPrices(stockNm['stockchoice'], stockNm['from_date'], stockNm['to_date'])})
 
 @app.route("/getsentiment", methods=['POST'])
 def getsentiment():
-    s=request.get_json()
-
-    sentimentdata=pd.read_sql(f"""select ticker, created_dt traded_dt, title, selftext, comments
-                                    from setup.reddit_commentary 
-                                    where ticker='{s['ticker']}' 
-                                    and created_dt between '{s['from_date']}' and '{s['to_date']}' 
-                                    order by created_dt desc""", db)
-
-    sentimentdata['merged_comments']=sentimentdata[['title','selftext','comments']].stack().groupby(level=0).apply(' '.join)
-
-    return jsonify({"sentiment": sentimentdata[['ticker','traded_dt','merged_comments']].to_dict(orient='list')})
+    sent=request.get_json()
+    return jsonify({"sentiment": bench.SentimentData(sent['ticker'], sent['from_date'], sent['to_date'])})
 
 
 
